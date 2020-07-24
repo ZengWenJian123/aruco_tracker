453f75e7322ce331f8f17f2a6e6c91484ae5165f
zengwenjian123 <838744447@qq.com>
2020-07-24 15:07:53 +0800

    估计aruco标记的位置

--- ar_reconstruction.py
+++ ar_reconstruction.py
@@ -1,3 +1,38 @@
 import cv2
 import glob
+import cv2.aruco as aruco
 
+
+frame_name=glob.glob('./pic/*.jpg')#搜索目录下所有的jpg图片
+for fname in frame_name:
+    img=cv2.imread(fname)
+    img=cv2.resize(img,None,fx=1,fy=1,interpolation=cv2.INTER_CUBIC)
+    #灰度化
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #设置预定义字典
+    aruco_dict=aruco.Dictionary_get(aruco.DICT_6X6_250)
+    # 使用默认值初始化检测器参数
+    parameters = aruco.DetectorParameters_create()
+    # 使用aruco.detectMarkers()函数可以检测到marker，返回ID和标志板的4个角点坐标
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+
+    print('corner:',corners,'ids:',ids,'rejectedImgPoints',rejectedImgPoints)
+    # 将detectMarkers函数返回的标记轮廓[0][0]点numpy数组转换为int
+
+    for i in range(ids.size):
+        print(i)
+        print(ids[i])
+        # 将detectMarkers函数返回的标记轮廓[0][0]点numpy数组转换为int
+        print(int(corners[i][0][0].tolist()[0]), int(corners[i][0][0].tolist()[1]))
+        cv2.circle(img, (int(corners[i][0][0].tolist()[0]), int(corners[i][0][0].tolist()[1])), 5, (0, 0, 255), -1)
+        cv2.putText(img, str(ids[i]), (int(corners[i][0][0].tolist()[0]), int(corners[i][0][0].tolist()[1])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+    # 画出标志位置
+    aruco.drawDetectedMarkers(img, corners, ids)
+
+
+
+
+
+    cv2.imshow('frame',img)
+    cv2.waitKey(1200)
